120005d413bfd38f01a2b4c33958ca793c6ac6cc
elprimo1337 <qwerty112478877@mail.ru>
2025-04-16 16:08:55 +0300

    нормальный хоар, битсорт, пирамидальная, шел сорт обрезанный. в хоаре и битсорте консты расставил.

---
+++ ads/against bitsort/against bitsort.cpp
@@ -0,0 +1,121 @@
+﻿#include <iostream>
+#include <vector>
+#include <chrono>
+#include <fstream>
+#include <stack>
+
+// Структура для хранения информации о подзадаче
+struct Subtask {
+    int l;
+    int r;
+    int k;
+};
+
+// Функция для проверки k - го бита числа
+bool checkBit(const int& n, const int& k) {
+    // Проверяем, является ли k-й бит установленным (равным 1)
+    if ((n >> k) & 1) {
+        return true; // Бит равен 1
+    }
+    else {
+        return false; // Бит равен 0
+    }
+}
+
+void bitsort(std::vector<int>& arr, const int& l, const int& r, const int& k) {
+    if (arr.empty()) return;
+
+    // 1. Инициализация стека
+    std::stack<Subtask> taskStack;
+    taskStack.push({ 0, static_cast<int>(arr.size() - 1), k });
+
+    // 2. Итеративная обработка
+    while (!taskStack.empty()) {
+        // a. Извлечение задачи
+        Subtask currentTask = taskStack.top();
+        taskStack.pop();
+
+        const int l = currentTask.l;
+        const int r = currentTask.r;
+        const int k = currentTask.k;
+
+        // b. все рассмотрели
+        if (l >= r || k < 0) {
+            continue; // Ничего не делаем
+        }
+
+        // c. Разделение
+        int i = l;
+        int j = r;
+        while (i <= j) {
+            // i движется вправо, пока не найдет бит k равный 1
+            while (i <= j && !checkBit(arr[i], k)) {
+                i++;
+            }
+
+            // j движется влево, пока не найдет бит k равный 0
+            while (i <= j && checkBit(arr[j], k)) {
+                j--;
+            }
+
+            // Если i и j не пересеклись, меняем местами элементы
+            if (i < j) {
+                std::swap(arr[i], arr[j]);
+                i++;
+                j--;
+            }
+        }
+
+        // d. Добавление подзадач (если они нужны)
+        if (i <= r && k > 0) {
+            taskStack.push({ i, r, k - 1 }); // И ТУТ
+        }
+        if (l <= j && k > 0) {
+            taskStack.push({ l, j, k - 1 }); // И ТУТ
+        }
+    }
+}
+
+int main() {
+        std::setlocale(LC_ALL, "Russian");
+
+        std::string filename;
+        std::cout << "Введите имя файла с числами: ";
+        std::cin >> filename;
+
+        std::ifstream inputFile(filename);
+
+        if (!inputFile.is_open()) {
+            std::cerr << "Ошибка открытия файла: " << filename << std::endl;
+            return 1;
+        }
+
+        std::vector<int> numbers;
+        int number;
+        while (inputFile >> number) {
+            numbers.push_back(number);
+        }
+
+        inputFile.close();
+
+        std::cout << "Считано " << numbers.size() << " чисел из файла." << std::endl;
+
+        // Замер времени сортировки
+        auto start = std::chrono::high_resolution_clock::now();
+        bitsort(numbers, 0, numbers.size() - 1, 31); // Сортировка вектора
+        auto end = std::chrono::high_resolution_clock::now();
+
+        std::chrono::duration<double> duration = end - start;
+
+        std::cout << "Время сортировки: " << duration.count() << " секунд" << std::endl;
+
+        // Вывод отсортированного массива (опционально)
+       /* std::cout << "Отсортированный массив: ";
+        for (int num : numbers) {
+            std::cout << num << " ";
+        }
+        std::cout << std::endl;
+       */
+        return 0;
+}
+
---
+++ ads/laba 4 piramid/laba 4 piramid.cpp
@@ -0,0 +1,125 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <locale>
+#include <chrono>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <stack> // Для итеративной реализации
+
+
+// Функция разделения
+int partition(std::vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j < high; ++j) {
+        if (arr[j] <= pivot) {
+            i++;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+// Итеративная функция быстрой сортировки
+void quickSort(std::vector<int>& arr) {
+    std::stack<std::pair<int, int>> st;
+    st.push({ 0, arr.size() - 1 });
+
+    while (!st.empty()) {
+        int low = st.top().first;
+        int high = st.top().second;
+        st.pop();
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            // Сначала кладем в стек больший подмассив
+            if (pi - low > high - pi) {
+                st.push({ low, pi - 1 });
+                st.push({ pi + 1, high });
+            }
+            else {
+                st.push({ pi + 1, high });
+                st.push({ low, pi - 1 });
+            }
+
+        }
+    }
+}
+
+
+
+// Функция проверки отсортированности массива
+template <typename T>
+bool isSorted(const std::vector<T>& arr) {
+    for (size_t i = 1; i < arr.size(); ++i) {
+        if (arr[i] < arr[i - 1]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Функция для чтения массива из файла
+std::vector<int> readArrayFromFile(const std::string& filename) {
+    std::vector<int> arr;
+    std::ifstream file(filename);
+    if (file.is_open()) {
+        int val;
+        while (file >> val) { // Читаем числа, пока не достигнем конца файла
+            arr.push_back(val);
+        }
+        file.close();
+    }
+    else {
+        std::cerr << "Unable to open file: " << filename << std::endl;
+    }
+    return arr;
+}
+
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    std::string filename = "array_1000000_-100000_100000.txt";
+
+    // Читаем массив из файла
+    std::vector<int> originalArray = readArrayFromFile(filename);
+    if (originalArray.empty()) {
+        std::cerr << "Error: Could not read array from file: " << filename << std::endl;
+        return 1;
+    }
+
+    // Создаем копию для сортировки
+    std::vector<int> arrayToSort = originalArray;
+    std::cout << "Размер массива: " << arrayToSort.size() << std::endl;
+
+
+    // Измеряем время сортировки
+    auto start = std::chrono::high_resolution_clock::now();
+    quickSort(arrayToSort);
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+
+    // Проверяем, отсортирован ли массив
+    bool sorted = isSorted(arrayToSort);
+
+
+    // Выводим результаты
+    std::cout << "----------------------------------------------------------" << std::endl;
+    std::cout << std::setw(30) << std::left << "Алгоритм:" << "Быстрая сортировка" << std::endl;
+    std::cout << std::setw(30) << std::left << "Размер массива:" << originalArray.size() << std::endl;
+    std::cout << std::setw(30) << std::left << "Отсортирован:" << (sorted ? "Да" : "Нет") << std::endl;
+    std::cout << std::setw(30) << std::left << "Среднее время сортировки:" << duration.count() << " мс" << std::endl;
+    if (originalArray.size() <= 20) {
+        std::cout << "Отсортированный массив: ";
+        for (const auto& elem : arrayToSort) {
+            std::cout << elem << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "----------------------------------------------------------" << std::endl << std::endl;
+
+    return 0;
+}
